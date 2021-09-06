698d079fa34e3497e40042e239c6019c90f52e07
Onaconnapuna <cjgermain12@gmail.com>
2021-09-06 17:52:22 -0400

    pieces placed, working on basic move functionality

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -54,12 +54,37 @@ class View {
     }
 
     placePieces() {
-        let squares = Array.from(document.getElementsByTagName('li'));
-        let first = squares[0];
-        let piece = document.createElement('div');
-        piece.innerHTML = '&#9812'
-        
-        first.appendChild(piece)
+
+        const whitePieces = ['&#9814', '&#9816', '&#9815', '&#9813', '&#9812', '&#9815', '&#9816', '&#9814']
+        const whitePawn = '&#9817'
+
+        const blackPieces = ['&#9820', '&#9822', '&#9821', '&#9818', '&#9819', '&#9821', '&#9822', '&#9820']
+        const blackPawn = '&#9823'
+
+        let squares = document.getElementsByTagName('li');
+
+        for (let i = 0; i < squares.length; i++) {
+            if (i < 8) {
+                let piece = document.createElement('div');
+                piece.innerHTML = blackPieces[i];
+                squares[i].appendChild(piece)
+            } else if (i > 7 && i < 16) {
+                let piece = document.createElement('div');
+                piece.innerHTML = blackPawn
+                squares[i].appendChild(piece)
+            } else if (i > 47 && i < 56) {
+                let piece = document.createElement('div');
+                piece.innerHTML = whitePawn
+                squares[i].appendChild(piece)
+            } else if (i > 55) {
+                // for(let j = 0; j < 8; j++) {
+                    let piece = document.createElement('div');
+                    piece.innerHTML = whitePieces[i % 56]
+                    squares[i].appendChild(piece)
+                // }
+            }
+        }
+
     }
 }
 

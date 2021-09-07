85347fe522962bd4ee1ed2405932e0129a30ad94
Onaconnapuna <cjgermain12@gmail.com>
2021-09-07 16:05:15 -0400

    fixed bug that was preventing display of pieces on move bar

--- src/scripts/best_moves.js
+++ src/scripts/best_moves.js
@@ -1,23 +1,26 @@
+import { async } from "regenerator-runtime";
+
 class BestMoves {
     constructor(viewEl, fenString) {
         this.viewEl = viewEl;
-        this.bestMoves = this.fetchBestMoves(fenString)
+        // this.bestMoves = this.fetchBestMoves(fenString);
         // setTimeout(() => {
-        //     this.displayMovePiece
+        //     this.displayBestMoves()
         // }, 1)
-        this.displayMovePiece()
+        this.displayBestMoves(fenString);
     }
 
     fetchBestMoves = async(fenString) => {
 
         const movesReference = document.createElement('ul');
         this.viewEl.appendChild(movesReference);
-        movesReference.setAttribute("class", "movesReference");
+        movesReference.setAttribute("class", "MovesReference");
 
         
         await fetch('https://explorer.lichess.ovh/master?' + `${fenString}`)
             .then((response) => response.json())
             .then((data) => {
+                //ebugger
                 for(let i = 0; i < data.moves.length; i++) {
                     let move = document.createElement('li');
                     move.setAttribute('id', `${data.moves[i].uci}`);
@@ -27,20 +30,23 @@ class BestMoves {
         
     }
 
-    displayMovePiece() { 
+    displayBestMoves = async(fenString) => { 
 
-        // setTimeout(this.fetchBestMoves(fenString))
+        await this.fetchBestMoves(fenString);
 
-        let table = document.getElementsByClassName('movesReference');
+        debugger
+
+        let table = document.getElementsByClassName('MovesReference');
         let moveIcons = table[0].childNodes;
 
         for(let i = 0; i < moveIcons.length; i++) {
-            firstPosID = moveIcons[i].id.split('').slice(0, 2).join('')
-            boardPos = document.getElementById(firstPosID);
+            let firstPosID = moveIcons[i].id.split('').slice(0, 2).join('')
+            let boardPos = document.getElementById(firstPosID);
             let piece = document.createElement('div');
             piece.innerHTML = `${boardPos.firstChild.innerHTML}`;
             moveIcons[i].appendChild(piece);
         }
+        console.log('I fired')
     }
 
 }
